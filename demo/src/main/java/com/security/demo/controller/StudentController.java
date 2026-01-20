c7b8a7624e3ebaa3e75d27fb3d0883709b3eab78
a6hijeet <abhijeetsatpute.dev@gmail.com>
2026-01-20 11:11:43 +0530

    Post request with CSRF token

--- demo/src/main/java/com/security/demo/controller/StudentController.java
+++ demo/src/main/java/com/security/demo/controller/StudentController.java
@@ -2,9 +2,17 @@ package com.security.demo.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import com.security.demo.model.Student;
+
+import jakarta.servlet.http.HttpServletRequest;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -24,5 +32,16 @@ public class StudentController {
       return students;
   }
 
+  @PostMapping("/students")
+  public Student addStudent(@RequestBody Student student) {
+    students.add(student);
+    return student;
+  }
+
+  @GetMapping("/csrf-token")
+  public CsrfToken getCsrfToken(HttpServletRequest request) {
+      return (CsrfToken) request.getAttribute("_csrf");
+  }
+  
   
 }
