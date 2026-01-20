d2f81ed7c346fc493a7efffaf442209a5f92630c
a6hijeet <abhijeetsatpute.dev@gmail.com>
2026-01-20 12:11:00 +0530

    Customized security filter chain

---
+++ demo/src/main/java/com/security/demo/config/ScurityConfig.java
@@ -0,0 +1,33 @@
+package com.security.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ScurityConfig {
+  
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+    // Disable CSRF
+    return http.csrf(customizer -> customizer.disable())
+        // Add authentication
+        .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+        // Add form login for browser
+        // disabling form login to enable stateless session
+        // This will add http authentication
+        /* .formLogin(Customizer.withDefaults()) */
+        // enable form login for api eg. postman
+        .httpBasic(Customizer.withDefaults())
+        // Making each request stateless
+        // it will generate new session id for each request.
+        .sessionManagement(session 
+          -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .build();
+  }
+}
--- demo/src/main/java/com/security/demo/controller/HelloController.java
+++ demo/src/main/java/com/security/demo/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.security.demo;
+package com.security.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
