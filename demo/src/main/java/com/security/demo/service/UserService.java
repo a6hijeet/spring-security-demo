a248bfe0c12248efeb2bc652dab04ce0baf6f9c0
a6hijeet <abhijeetsatpute.dev@gmail.com>
2026-01-21 09:57:48 +0530

    Added BCryptPasswordEncoder

--- demo/src/main/java/com/security/demo/service/UserService.java
+++ demo/src/main/java/com/security/demo/service/UserService.java
@@ -1,6 +1,7 @@
 package com.security.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.security.demo.model.Users;
@@ -12,7 +13,9 @@ public class UserService {
   @Autowired
   private UserRepo repo;
 
+  BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
   public Users register(Users user) {
+    user.setPassword(encoder.encode(user.getPassword()));
     return repo.save(user);
   }
 }
